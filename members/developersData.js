f55bce2c9a57d53c13ca66c822c82aac943bc4e4
mbesseygodwin@gmail.com <mbesseygodwin@gmail.com>
2024-04-21 23:49:43 +0100

    added members page

---
+++ members/developersData.js
@@ -0,0 +1,29 @@
+  // Data for developers
+  const developersData = [
+    {
+        name: "Mbessey Godwin - Fullstack",
+        image: "./images/nelson.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque."
+    },
+    // Add more developer objects as needed
+    {
+        name: "Onyedikachi Ezeobi - Backend",
+        image: "./images/nelson.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque."
+    },
+    {
+        name: "Amos Edidong - Backend",
+        image: "./images/nelson.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque."
+    },
+    {
+        name: "Auston Uoc - Frontend",
+        image: "./images/nelson.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque."
+    },
+    {
+        name: "Mark Abdul - Mobile",
+        image: "./images/nelson.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat quos in accusamus deleniti voluptatibus quo velit similique tempore nihil, iure facere ducimus, doloribus, cupiditate minima dolor culpa eum quis. Atque."
+    },
+];
---
+++ members/members.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const developersContainer = document.getElementById('developersContainer');
+
+    // Function to create developer card
+    function createDeveloperCard(developer) {
+        const card = document.createElement('div');
+        card.classList.add('row', 'p-1');
+
+        const cardContent = `
+            <div class="col-12 m-0 p-0">
+                <div class="sm:flex">
+                    <div class="p-0">
+                        <img src="${developer.image}" alt="" class="border border border-dark hidden sm:inline-block" style="width: 600px;">
+                    </div>
+                    <div class="ml-1">
+                        <h1 class="form-control tracking-tight mb-1 text-xs lg:text-lg border border border-dark fw-bold text-capitalize">${developer.name}</h1>
+                        <p class="form-control small text-xs lg:text-lg border border border-dark">${developer.description}</p>
+                    </div>
+                </div>
+            </div>
+            <hr class="my-2 border border-light">
+        `;
+        card.innerHTML = cardContent;
+
+        return card;
+    }
+
+    // Function to render developers
+    function renderDevelopers() {
+        developersData.forEach(developer => {
+            const developerCard = createDeveloperCard(developer);
+            developersContainer.appendChild(developerCard);
+        });
+    }
+
+    // Initialize rendering
+    renderDevelopers();
+});
